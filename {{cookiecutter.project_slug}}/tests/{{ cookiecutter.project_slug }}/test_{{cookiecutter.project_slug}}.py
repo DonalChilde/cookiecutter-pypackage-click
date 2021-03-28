ba9471d2b965b9b32467be2daa9f616eb3b30359
Chad Lowe <pfmsoft.dev@gmail.com>
2021-03-28 11:08:25 -0700

    added test for hello

--- {{cookiecutter.project_slug}}/tests/{{ cookiecutter.project_slug }}/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/{{ cookiecutter.project_slug }}/test_{{cookiecutter.project_slug}}.py
@@ -22,3 +22,13 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
     assert "--help  Show this message and exit." in help_result.output
+
+def test_hello():
+    """Test the hello command."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ["hello", "Foo"])
+    assert result.exit_code == 0
+    assert "Hello Foo" in result.output
+    help_result = runner.invoke(cli.main, ["--help"])
+    assert help_result.exit_code == 0
+    assert "--help  Show this message and exit." in help_result.output
