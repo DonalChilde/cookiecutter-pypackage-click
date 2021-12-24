3a813f05cd66ddfb66773e87cc959373a05854a5
Chad Lowe <pfmsoft.dev@gmail.com>
2021-12-24 12:47:25 -0700

    refactor example resources

--- {{cookiecutter.project_slug}}/tests/{{ cookiecutter.project_slug }}/test_resources.py
+++
@@ -1,4 +0,0 @@
-def test_example_resource(example_resource):
-    """Check that the example resource was loaded correctly."""
-    assert len(example_resource) == 4
-    assert example_resource[0]["average"] == 6.29
